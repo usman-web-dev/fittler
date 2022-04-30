8541d8715215e991b28663e1313719695d535c38
Usman Dev <usman.developer002@gmail.com>
2022-04-30 16:06:11 +0500

    Added daily and weekly leaderboards.

--- src/pages/leaderboard/index.vue
+++ src/pages/leaderboard/index.vue
@@ -1,18 +1,39 @@
 <template>
-  <v-simple-table class="rounded-xl shadow">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Calories Burnt</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in usersData" :key="item.name">
-        <td>{{ item.name }}</td>
-        <td>{{ item.calories }}</td>
-      </tr>
-    </tbody>
-  </v-simple-table>
+  <div>
+    <h1>Daily Leaderboard</h1>
+
+    <v-simple-table class="rounded-xl shadow">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Calories Burnt</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in usersData" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+
+    <h1 class="mt-5">Weekly Leaderboard</h1>
+
+    <v-simple-table class="rounded-xl shadow">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Calories Burnt</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in usersData" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+  </div>
 </template>
 
 <script lang="ts" src="./index.ts" />
